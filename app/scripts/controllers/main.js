6ae016fd06cf08b3a5a209b14e6f71fd8e821f43
Armann David Sigurdsson <armannds@gmail.com>
2017-12-26 14:27:22 +0000

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name weatherForecasterApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the weatherForecasterApp
+ */
+angular.module('weatherForecasterApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
