0d7854380df7034ea4cf63fcd60c4c04e2867d41
Armann David Sigurdsson <armannds@gmail.com>
2018-01-24 22:28:56 +0100

    Update user exp on home page

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -2,11 +2,16 @@
   'use strict';
 
   angular.module('weatherForecasterApp')
-    .controller('HomeCtrl', ['$scope', 'cityService', function ($scope, cityService) {
+    .controller('HomeCtrl', ['$scope', '$location', 'cityService', function ($scope, $location, cityService) {
 
       $scope.city = cityService.city;
+
       $scope.$watch('city', function () {
         cityService.city = $scope.city;
       });
+
+      $scope.submit = function() {
+        $location.path('/forecast');
+      };
     }]);
 })();
